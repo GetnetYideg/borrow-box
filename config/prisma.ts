3c1de7e00a5b97a951f80522128ca096882db637
Getnet Yideg <getnetyideg54@gmail.com>
2026-09-12 14:12:37 +0300

    updated export mechanism to default export

--- config/prisma.ts
+++ config/prisma.ts
@@ -6,4 +6,6 @@ const { Pool } = pkg;
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 const adapter = new PrismaPg(pool);
 
-export const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient({ adapter });
+
+export default prisma;
