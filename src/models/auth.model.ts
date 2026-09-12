cb7fe061b98f321c3b80d8c587f69d91049fece1
Getnet Yideg <getnetyideg54@gmail.com>
2026-09-12 23:55:01 +0300

    added user input models

---
+++ src/models/auth.model.ts
@@ -0,0 +1,10 @@
+export interface createAccountInput{
+    name: string,
+    email: string,
+    password: string
+}
+
+export interface loginAccountInput{
+    email: string,
+    password: string
+}
