a982850a630a7f1e3bd5897493c37a3436d600bc
Getnet Yideg <getnetyideg54@gmail.com>
2026-09-13 10:34:37 +0300

    added registration validation schema

---
+++ src/utils/auth.validation.ts
@@ -0,0 +1,33 @@
+import Joi from 'joi'
+
+const corporateEmailRegex = /^[a-zA-Z0-9._%+-]+@company\.com$/;
+
+export const createAccountSchema = Joi.object({
+    name: Joi
+        .string()
+        .trim()
+        .min(1)
+        .max(255).
+        required(),
+    email: Joi
+        .string()
+        .lowercase()
+        .pattern(corporateEmailRegex)
+        .required()
+        .messages({
+            'string.pattern.base': 'Email must be a valid internal @company.com address.',
+            'string.empty': 'Email cannot be left blank.',
+            'any.required': 'Email is required.'
+        }),
+    password: Joi
+        .string()
+        .min(8)
+        .max(50)
+        .required()
+        .messages({
+            'string.min': 'Password must be at least 8 characters long.',
+            'string.max': 'Password cannot exceed 30 characters.',
+            'string.empty': 'Password cannot be empty.',
+            'any.required': 'Password is a required field.'
+        })
+})
