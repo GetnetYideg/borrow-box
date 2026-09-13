a3a8ff0786646dde93d49af260eb171926439bb3
Getnet Yideg <getnetyideg54@gmail.com>
2026-09-13 11:09:44 +0300

    added registration api to auth routes

---
+++ src/routes/auth.routes.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express'
+import { validateBody } from '../middlewares/validation.middleware.js'
+import { createAccountSchema } from '../utils/auth.validation.js';
+import { register } from '../controllers/auth.controller.js';
+
+const authRouter = Router();
+
+authRouter.post('/', validateBody(createAccountSchema), register);
