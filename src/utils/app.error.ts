74fb4a6f294710820428cbb96d7b5776d975101b
Getnet Yideg <getnetyideg54@gmail.com>
2026-09-13 10:16:15 +0300

    added error class

---
+++ src/utils/app.error.ts
@@ -0,0 +1,11 @@
+export class AppError extends Error {
+  public readonly statusCode: number;
+  public readonly isOperational = true;
+
+  constructor(statusCode: number, message: string) {
+    super(message);
+    this.statusCode = statusCode;
+    Object.setPrototypeOf(this, AppError.prototype);
+    Error.captureStackTrace?.(this, this.constructor);
+  }
+}
