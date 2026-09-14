0e87df5c7bf2bf753621b5f502c2dfe078d860ac
Getnet Yideg <getnetyideg54@gmail.com>
2026-09-14 16:28:37 +0300

    added login route

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express'
 import { validateBody } from '../middlewares/validation.middleware.js'
-import { createAccountSchema } from '../utils/auth.validation.js';
-import { register } from '../controllers/auth.controller.js';
+import { createAccountSchema, loginAccountSchema } from '../utils/auth.validation.js';
+import { register, login } from '../controllers/auth.controller.js';
 
 const authRouter = Router();
 
 authRouter.post('/register', validateBody(createAccountSchema), register);
+authRouter.post('/login', validateBody(loginAccountSchema), login)
 
 export default authRouter;
