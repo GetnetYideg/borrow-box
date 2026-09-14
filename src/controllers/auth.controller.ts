31cd052dbb3f8bed8959d4fa5205ab6b89f2c672
Getnet Yideg <getnetyideg54@gmail.com>
2026-09-14 16:29:25 +0300

    added login login controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -6,9 +6,32 @@ export const register = async (
     res: Response, 
     next: NextFunction): Promise<void> =>{
         try{
-            const user = await authService.register(req.body);
+            const user = await authService.registerationService(req.body);
             res.status(201).json(user);
         }catch(error){
             next(error);
         }
 }
+
+export const login = async (
+    req: Request,
+    res: Response,
+    next: NextFunction): Promise<void> =>{
+        try{
+            const info = await authService.loginService(req.body);
+            const { data, accessToken, refreshToken } = info;
+            res.cookie("refreshToken", refreshToken, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === "production",
+                sameSite: 'lax',
+                maxAge: 7 * 24 * 60 * 60 * 1000
+            })
+            
+            res.status(200).json({
+                data, 
+                accessToken
+            });
+        }catch(error){
+            next(error)
+        }
+    }
