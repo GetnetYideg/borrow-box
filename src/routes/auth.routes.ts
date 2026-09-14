a00750b15c7a5172e9272daef6976a053ef5fd44
Getnet Yideg <getnetyideg54@gmail.com>
2026-09-14 20:06:37 +0300

    added refresh route

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express'
 import { validateBody } from '../middlewares/validation.middleware.js'
 import { createAccountSchema, loginAccountSchema } from '../utils/auth.validation.js';
-import { register, login, logout } from '../controllers/auth.controller.js';
+import { register, login, logout, refreshAccessToken } from '../controllers/auth.controller.js';
 import authenticateToken from "../middlewares/auth.middleware.js"
 
 const authRouter = Router();
@@ -9,5 +9,6 @@ const authRouter = Router();
 authRouter.post('/register', validateBody(createAccountSchema), register);
 authRouter.post('/login', validateBody(loginAccountSchema), login)
 authRouter.post('/logout', authenticateToken, logout)
+authRouter.post('/refresh', refreshAccessToken)
 
 export default authRouter;
