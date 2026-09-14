ad3c08fc03d44bfdf7a4bc0839d3ad502b7c4dcc
Getnet Yideg <getnetyideg54@gmail.com>
2026-09-14 19:06:37 +0300

    added logout route

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express'
 import { validateBody } from '../middlewares/validation.middleware.js'
 import { createAccountSchema, loginAccountSchema } from '../utils/auth.validation.js';
-import { register, login } from '../controllers/auth.controller.js';
+import { register, login, logout } from '../controllers/auth.controller.js';
+import authenticateToken from "../middlewares/auth.middleware.js"
 
 const authRouter = Router();
 
 authRouter.post('/register', validateBody(createAccountSchema), register);
 authRouter.post('/login', validateBody(loginAccountSchema), login)
+authRouter.post('/logout', authenticateToken, logout)
 
 export default authRouter;
