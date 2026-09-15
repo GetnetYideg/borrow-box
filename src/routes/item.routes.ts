1659bd10ea44de91be79514df3119d88bcdb06f7
Getnet Yideg <getnetyideg54@gmail.com>
2026-09-15 23:43:37 +0300

    added fetch all items endpoint

--- src/routes/item.routes.ts
+++ src/routes/item.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express'
-import { createItem } from "../controllers/item.controller.js";
+import { createItem, getAllItems } from "../controllers/item.controller.js";
 import authenticateToken from "../middlewares/auth.middleware.js";
 
 const itemRouter = Router();
 
 itemRouter.post('/', authenticateToken, createItem);
+itemRouter.get('/', authenticateToken, getAllItems)
 
 export default itemRouter;
