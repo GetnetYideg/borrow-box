b33409b337e2cee0f1f915b4a278994d4c8122fb
Getnet Yideg <getnetyideg54@gmail.com>
2026-09-15 23:17:52 +0300

    added item creation service

---
+++ src/services/item.service.ts
@@ -0,0 +1,35 @@
+import "dotenv/config";
+import prisma from "../../config/prisma.js";
+import { AppError } from "../utils/app.error.js";
+import { createItemInput } from "../models/item.model.js";
+import { connect } from "http2";
+
+export const createItemService = async (data: createItemInput, userId: string) =>{
+    try {
+        const { name, description, category, imageUrl, identifier, status } = data;
+
+        if( !name || !status ){
+            throw new AppError(400, "Insert required fields");
+        }
+
+        return await prisma.item.create({
+            data: {
+                name, 
+                description,
+                category,
+                imageUrl,
+                identifier,
+                status,
+                owner: {
+                    connect: {
+                        id: userId
+                    }
+                }
+            }
+        })
+    } catch (error) {
+        if (error instanceof AppError) throw error;
+        console.error("Registration faild:", error);
+        throw error;
+    }
+}
