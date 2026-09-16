a859e7ed68d65adb24bb50789803003d748caae4
Getnet Yideg <getnetyideg54@gmail.com>
2026-09-16 14:54:36 +0300

    added search item endpoint

--- src/routes/item.routes.ts
+++ src/routes/item.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express'
-import { createItem, getAllItems } from "../controllers/item.controller.js";
+import { createItem, getAllItems, searchItem } from "../controllers/item.controller.js";
 import authenticateToken from "../middlewares/auth.middleware.js";
 
 const itemRouter = Router();
 
 itemRouter.post('/', authenticateToken, createItem);
-itemRouter.get('/', authenticateToken, getAllItems)
+itemRouter.get('/', authenticateToken, getAllItems);
+itemRouter.get('/:id', authenticateToken, searchItem)
 
 export default itemRouter;
