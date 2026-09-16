c9270de2282ccc5b2ab5fb17c4af17ad20245026
Getnet Yideg <getnetyideg54@gmail.com>
2026-09-16 15:31:40 +0300

    added delete item endpoint

--- src/routes/item.routes.ts
+++ src/routes/item.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express'
-import { createItem, getAllItems, searchItem } from "../controllers/item.controller.js";
+import { createItem, deleteItem, getAllItems, searchItem } from "../controllers/item.controller.js";
 import authenticateToken from "../middlewares/auth.middleware.js";
 
 const itemRouter = Router();
 
 itemRouter.post('/', authenticateToken, createItem);
 itemRouter.get('/', authenticateToken, getAllItems);
-itemRouter.get('/:id', authenticateToken, searchItem)
+itemRouter.get('/:id', authenticateToken, searchItem);
+itemRouter.delete('/:id', authenticateToken, deleteItem);
 
 export default itemRouter;
