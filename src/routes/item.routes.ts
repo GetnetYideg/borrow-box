1b78d15b02f4678991e706a21ab582390f04edec
Getnet Yideg <getnetyideg54@gmail.com>
2026-09-17 23:11:01 +0300

    added filter by category endpoint

--- src/routes/item.routes.ts
+++ src/routes/item.routes.ts
@@ -1,11 +1,23 @@
-import { Router } from 'express'
-import { createItem, deleteItem, getAllItems, searchItem } from "../controllers/item.controller.js";
+import { Request, Response, NextFunction, Router } from 'express'
+import { createItem, deleteItem, filterByCategory, getAllItems, searchItem } from "../controllers/item.controller.js";
 import authenticateToken from "../middlewares/auth.middleware.js";
+
+interface newRequest extends Request {
+  user?: {
+    id: string;
+    name: string;
+    email: string;
+  };
+}
 
 const itemRouter = Router();
 
 itemRouter.post('/', authenticateToken, createItem);
-itemRouter.get('/', authenticateToken, getAllItems);
+itemRouter.get('/', authenticateToken, (req: newRequest, res: Response, next: NextFunction) =>{
+    if(req.query.category) return filterByCategory(req, res, next);
+
+    return getAllItems(req, res, next);
+});
 itemRouter.get('/:id', authenticateToken, searchItem);
 itemRouter.delete('/:id', authenticateToken, deleteItem);
 
