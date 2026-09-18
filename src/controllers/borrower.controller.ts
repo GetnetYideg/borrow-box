9be5c7e2854b1b252feca0cbe287619161bd4bea
Getnet Yideg <getnetyideg54@gmail.com>
2026-09-18 15:57:34 +0300

    added list borrowers controller

--- src/controllers/borrower.controller.ts
+++ src/controllers/borrower.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response, NextFunction } from "express";
 import { AppError } from "../utils/app.error.js";
 import * as borrowerService from "../services/borrower.service.js"
+import { truncate } from "fs";
+import app from "../app.js";
 
 interface newRequest extends Request{
     user?:{
@@ -20,6 +22,22 @@ export const createBorrower = async (
     
             res.status(200).json(borrower);
             
+        } catch (error) {
+            next(error)
+        }
+    }
+export const getAllBorrowers = async(
+    req: newRequest,
+    res: Response,
+    next: NextFunction): Promise<void> =>{
+        try {
+            const userId = req.user?.id;
+            if(!userId){
+                throw new AppError(401, "Unauthorized");
+            }
+            const borrowers = await borrowerService.getAllBorrowersService(userId);
+
+            res.status(200).json(borrowers);
         } catch (error) {
             next(error)
         }
