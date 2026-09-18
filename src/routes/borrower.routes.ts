09d98b17191fb197ca13ced4cb7b09d81925f6d6
Getnet Yideg <getnetyideg54@gmail.com>
2026-09-18 17:20:44 +0300

    added delete router endpoint

--- src/routes/borrower.routes.ts
+++ src/routes/borrower.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createBorrower, getAllBorrowers, searchBorrower } from "../controllers/borrower.controller.js";
+import { createBorrower, deleteBorrower, getAllBorrowers, searchBorrower } from "../controllers/borrower.controller.js";
 import authenticateToken from "../middlewares/auth.middleware.js";
 import { createBorrowerSchema } from "../utils/borrower.validation.js";
 import { validateBody } from "../middlewares/validation.middleware.js";
@@ -9,5 +9,6 @@ const borrowerRouter = Router();
 borrowerRouter.post('/', authenticateToken, validateBody(createBorrowerSchema), createBorrower);
 borrowerRouter.get('/', authenticateToken, getAllBorrowers);
 borrowerRouter.get('/:id', authenticateToken, searchBorrower);
+borrowerRouter.delete('/:id', authenticateToken, deleteBorrower);
 
 export default borrowerRouter;
