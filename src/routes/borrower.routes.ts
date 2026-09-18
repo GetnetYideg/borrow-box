b6deb0d5b4fd7b93698c23cb0c79decd98444de8
Getnet Yideg <getnetyideg54@gmail.com>
2026-09-18 16:42:37 +0300

    added search borrower endpoint

--- src/routes/borrower.routes.ts
+++ src/routes/borrower.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createBorrower, getAllBorrowers } from "../controllers/borrower.controller.js";
+import { createBorrower, getAllBorrowers, searchBorrower } from "../controllers/borrower.controller.js";
 import authenticateToken from "../middlewares/auth.middleware.js";
 import { createBorrowerSchema } from "../utils/borrower.validation.js";
 import { validateBody } from "../middlewares/validation.middleware.js";
@@ -8,5 +8,6 @@ const borrowerRouter = Router();
 
 borrowerRouter.post('/', authenticateToken, validateBody(createBorrowerSchema), createBorrower);
 borrowerRouter.get('/', authenticateToken, getAllBorrowers);
+borrowerRouter.get('/:id', authenticateToken, searchBorrower);
 
 export default borrowerRouter;
