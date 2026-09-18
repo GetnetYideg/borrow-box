82855769ea989359694859050ae3a0dbb87d384e
Getnet Yideg <getnetyideg54@gmail.com>
2026-09-18 15:10:12 +0300

    added borrower router to main app

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import authRouter from './routes/auth.routes.js';
 import itemRouter from './routes/item.routes.js';
+import borrowerRouter from './routes/borrower.routes.js';
 
 const app = express();
 
@@ -10,5 +11,6 @@ app.use(cookieParser());
 
 app.use('/api/auth', authRouter);
 app.use('/api/item', itemRouter);
+app.use('/api/borrower', borrowerRouter);
 
 export default app;
