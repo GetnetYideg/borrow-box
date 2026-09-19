e3a37c2660cb364b0218f6c49df989f6d8bb64e0
Getnet Yideg <getnetyideg54@gmail.com>
2026-09-19 17:56:39 +0300

    added lend route to the main app

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cookieParser from 'cookie-parser';
 import authRouter from './routes/auth.routes.js';
 import itemRouter from './routes/item.routes.js';
 import borrowerRouter from './routes/borrower.routes.js';
+import lendingRouter from './routes/lending.routes.js';
 
 const app = express();
 
@@ -12,5 +13,6 @@ app.use(cookieParser());
 app.use('/api/auth', authRouter);
 app.use('/api/item', itemRouter);
 app.use('/api/borrower', borrowerRouter);
+app.use('/api/lend/', lendingRouter)
 
 export default app;
