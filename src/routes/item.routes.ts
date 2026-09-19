09ebddfd3ea4dba2772d24c089040a6b470d9fe7
Getnet Yideg <getnetyideg54@gmail.com>
2026-09-19 14:13:30 +0300

    added update item endpoint

--- src/routes/item.routes.ts
+++ src/routes/item.routes.ts
@@ -1,6 +1,8 @@
 import { Request, Response, NextFunction, Router } from 'express'
-import { createItem, deleteItem, filterByCategory, getAllItems, searchItem } from "../controllers/item.controller.js";
+import { createItem, deleteItem, filterByCategory, getAllItems, searchItem, updateItem } from "../controllers/item.controller.js";
 import authenticateToken from "../middlewares/auth.middleware.js";
+import { validateBody } from '../middlewares/validation.middleware.js';
+import { updateItemInputSchema } from '../utils/item.validation.js';
 
 interface newRequest extends Request {
   user?: {
@@ -19,6 +21,7 @@ itemRouter.get('/', authenticateToken, (req: newRequest, res: Response, next: Ne
     return getAllItems(req, res, next);
 });
 itemRouter.get('/:id', authenticateToken, searchItem);
+itemRouter.put('/:id', authenticateToken, validateBody(updateItemInputSchema), updateItem)
 itemRouter.delete('/:id', authenticateToken, deleteItem);
 
 export default itemRouter;
