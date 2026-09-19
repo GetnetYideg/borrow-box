4a3dccdd809d7c5877b7a915678739ad69714de5
Getnet Yideg <getnetyideg54@gmail.com>
2026-09-19 20:28:20 +0300

    added list history endpoint

--- src/routes/lending.routes.ts
+++ src/routes/lending.routes.ts
@@ -2,10 +2,11 @@ import { Router } from "express";
 import authenticateToken from "../middlewares/auth.middleware.js";
 import { validateBody } from "../middlewares/validation.middleware.js";
 import { lendingSchema } from "../utils/lending.validation.js";
-import { lendItem, returnItem } from "../controllers/lending.controller.js";
+import { lendItem, listHistory, returnItem } from "../controllers/lending.controller.js";
 
 const lendingRouter = Router();
 
+lendingRouter.get('/', authenticateToken, listHistory)
 lendingRouter.post('/', authenticateToken, validateBody(lendingSchema), lendItem);
 lendingRouter.put('/return/:id', authenticateToken, returnItem);
 
