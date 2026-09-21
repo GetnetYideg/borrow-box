ae41a12a4322b4acbed4bae2b269e0e32604cebe
Getnet Yideg <getnetyideg54@gmail.com>
2026-09-21 17:17:06 +0300

    added endpoint for tracking due date

--- src/routes/lending.routes.ts
+++ src/routes/lending.routes.ts
@@ -2,12 +2,13 @@ import { Router } from "express";
 import authenticateToken from "../middlewares/auth.middleware.js";
 import { validateBody } from "../middlewares/validation.middleware.js";
 import { lendingSchema } from "../utils/lending.validation.js";
-import { lendItem, listHistory, returnItem } from "../controllers/lending.controller.js";
+import { lendItem, listHistory, returnItem, trackDueDate } from "../controllers/lending.controller.js";
 
 const lendingRouter = Router();
 
 lendingRouter.get('/', authenticateToken, listHistory)
 lendingRouter.post('/', authenticateToken, validateBody(lendingSchema), lendItem);
 lendingRouter.put('/return/:id', authenticateToken, returnItem);
+lendingRouter.put('/track/:id', authenticateToken, trackDueDate)
 
 export default lendingRouter;
