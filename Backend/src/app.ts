e5b2622b519f06ee6822d9352fdf07e7348d867c
Getnet Yideg <getnetyideg54@gmail.com>
2026-09-26 12:06:53 +0300

    Updated the frontend link in cors

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -11,7 +11,7 @@ import messageRouter from './routes/message.routes.js';
 const app = express();
 
 app.use(cors({
-  origin: "https://borrow-box-sigma.vercel.app/",
+  origin: "https://borrow-box-moa24.vercel.app/",
   credentials: true,
 }));
 app.use(express.json());
